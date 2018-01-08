6d81785d5ab723c975555975aceb6455c059f10c
Jian Shen Yap <jianshen@dangodigital.com>
2018-01-08 23:19:16 +0800

    Did initial training. Need to fix maximum document length. wit’s api only accept up to 256 alphabet.

---
+++ wit/data_helper.py
@@ -0,0 +1,33 @@
+import numpy as np
+import re
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_tab_files(data_file):
+
+    # Load data from files
+    raw_data = list(open(data_file, "r", encoding="utf8").readlines())
+    raw_data = [s.split("\t") for s in raw_data]
+
+    data = [[clean_str(s[0]), s[1].strip('\n')] for s in raw_data]
+
+    return data
--- wit/training.py
+++ wit/training.py
@@ -1,44 +1,80 @@
 import requests
 import json
+from wit import data_helper
 
 # Wit.ai's API for training the app
 train_api = "https://api.wit.ai/samples?v=20170307"
 
-# Insert Code to Read Data
+# Custom Headers
+train_headers = {
+    "Authorization": "Bearer CQXYCLH5ZCIXF62DY3XJC33JOLXO2K5V",
+    "Content-Type": "application/json"
+}
 
-# Training Data
-train_payload = [
-    {
-        "text": "XD?",
+# Batch Size
+batch = 1
+
+# Read Data
+data = data_helper.load_tab_files('Training_data.txt')
+
+train_payload = []
+for idx, entry in enumerate(data):
+    temp_entry = {
+        "text": entry[0],
         "entities": [
             {
                 "entity": "intent",
-                "value": "BillAmount"
-            }
-        ]
-    },
-    {
-        "text": "hahaha?",
-        "entities": [
-            {
-                "entity": "intent",
-                "value": "BillAmount"
+                "value": entry[1]
             }
         ]
     }
-]
 
-# Jsonify Payload
-train_payload_json = json.dumps(train_payload)
+    train_payload.append(temp_entry)
 
-# Custom Headers
-train_headers = {
-    "Authorization" : "Bearer CQXYCLH5ZCIXF62DY3XJC33JOLXO2K5V",
-    "Content-Type" : "application/json"
-}
+    # Sending payload in batch
+    # if (idx+1) % batch == 0:
+    if idx < 30 and idx > 19:
+        # Jsonify Payload
+        train_payload_json = json.dumps(train_payload)
 
-r = requests.post(train_api, data = train_payload_json, headers=train_headers)
+        r = requests.post(train_api, data=train_payload_json, headers=train_headers)
 
-# Printing response
-# Might want to do response checking here
-print(r.text)
+        # Printing response
+        print("Data no.{} - no.{}".format(idx-batch+2,idx+1))
+        print(r.text)
+
+        # Recording Error
+        if r.status_code != 200:
+            file = open("log.txt","a")
+            file.write("Data no.{} - no.{}".format(idx-batch+2,idx+1))
+            file.write("\n")
+            file.write(r.text)
+            file.write("\n")
+            file.close
+            print("Error Recorded to Log!!")
+
+        # Reset Training Payload
+        train_payload = []
+
+# Training Data
+# train_payload = [
+#     {
+#         "text": "XD?",
+#         "entities": [
+#             {
+#                 "entity": "intent",
+#                 "value": "BillAmount"
+#             }
+#         ]
+#     },
+#     {
+#         "text": "hahaha?",
+#         "entities": [
+#             {
+#                 "entity": "intent",
+#                 "value": "BillAmount"
+#             }
+#         ]
+#     }
+# ]
+
